90bda270b9ecaf1307ae3d67cd72e85d376e013f
heber-minetto <heber.minetto@rminfraestrutura.com.br>
2017-09-10 19:34:46 -0300

    Versão e nome

--- openmcu-ru/files/scripts/tls_make_client.sh
+++ openmcu-ru/files/scripts/tls_make_client.sh
@@ -3,7 +3,7 @@
 CA_PATH="./CA"
 SSL_PATH="../ssl"
 COMMON_NAME="client"
-ORG_NAME="OpenMCU-ru"
+ORG_NAME="MCU-RedeConecta"
 
 if [ ! -z $1 ] ; then
   COMMON_NAME=$1
@@ -24,7 +24,7 @@ echo "
   basicConstraints=CA:TRUE
   subjectKeyIdentifier=hash
   authorityKeyIdentifier=keyid,issuer:always
-  nsComment=\"OpenMCU-ru client certificate\"
+  nsComment=\"OMCU-RedeConecta client certificate\"
   nsCertType=client
   extendedKeyUsage=clientAuth
 
